5d567f560823d96c04085b44d8355e586b947b98
eiyen <937915771@qq.com>
2023-09-01 16:24:03 +0800

    feat: 完成获取 VRF 的合约编写
    编码流程：
    
    1. 下载 Chainlink 库。
    2. 导入协调器合约，并配置路径。
    3. 调用 requestRandomWords 函数。
    4. 声明并添加函数参数。
    5. 完善函数注释。

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+
 /**
  * @dev 合约的实现逻辑如下：
  *
@@ -27,6 +29,14 @@ contract Raffle {
     uint256 private s_minimumOpenTime;
     address[] private s_ticketHolders;
 
+    VRFCoordinatorV2Interface private i_vrfCoordinator;
+    bytes32 private immutable i_maximumGasPrice;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+
+    uint32 private constant NUM_WORDS = 1;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+
     /* 3. 声明事件 */
     event TicketPurchased(
         uint256 indexed ticketHolderIndex,
@@ -40,14 +50,25 @@ contract Raffle {
     );
     error Raffle__RaffleNotOpen(RaffleStates);
     error Raffle__MinimumOpenTimeNotReached(uint256 timeRemaining);
+
     // error Raffle__MinimumOpenTimeNotReached();
 
     /* 5. 定义构造函数 */
-    constructor(uint256 ticketPrice, uint256 startTime, uint256 minimumOpenTime) {
+    constructor(
+        uint256 ticketPrice,
+        uint256 startTime,
+        uint256 minimumOpenTime,
+        address vrfCoordinator,
+        bytes32 maximumGasPrice,
+        uint64 subscriptionId
+    ) {
         s_ticketPrice = ticketPrice;
         s_raffleState = RaffleStates.OPEN;
         s_startTime = startTime;
         s_minimumOpenTime = minimumOpenTime;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinator);
+        i_maximumGasPrice = maximumGasPrice;
+        i_subscriptionId = subscriptionId;
     }
 
     /**
@@ -71,9 +92,30 @@ contract Raffle {
 
     function pickWinner() external payable {
         if (block.timestamp - s_startTime < s_minimumOpenTime) {
-            revert Raffle__MinimumOpenTimeNotReached(s_minimumOpenTime - (block.timestamp - s_startTime));
+            revert Raffle__MinimumOpenTimeNotReached(
+                s_minimumOpenTime - (block.timestamp - s_startTime)
+            );
             // revert Raffle__MinimumOpenTimeNotReached();
         }
+
+        /**
+         * @param i_maximumGasPrice 允许的最大 gas 价格，这里是 30 gWei
+         * @param i_subscriptionId 订阅的 VRF 账号ID，详见：https://vrf.chain.link/sepolia
+         * @param REQUEST_CONFIRMATIONS 经过确认的区块数，默认交易在3个区块后确认
+         * @param i_callbackGasLimit 回调函数的 gas 消耗上限
+         * @param NUM_WORDS 请求的随机数数量
+         * 
+         * @dev 回调函数是执行完 requestRandomWords 函数后，紧接着的 fulfillRandomWords 函数
+         * @dev 在这笔交易中，我们需要执行两个函数：requestRandomWords 和 fulfillRandomWords
+         * @dev 由于交易的原子性，任何一笔函数发生错误，整个交易都会失败。
+         */
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_maximumGasPrice,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
     }
 
     /**
@@ -88,16 +130,16 @@ contract Raffle {
         return s_ticketHolders[index];
     }
 
-    function getMinimumOpenTime() external view returns(uint256) {
+    function getMinimumOpenTime() external view returns (uint256) {
         return s_minimumOpenTime;
     }
 
-    function getStartTime() external view returns(uint256) {
+    function getStartTime() external view returns (uint256) {
         return s_startTime;
     }
 
     /* block.timestamp 测试 */
-    function getBlockTimestamp() external view returns(uint256) {
+    function getBlockTimestamp() external view returns (uint256) {
         return block.timestamp;
     }
 }
